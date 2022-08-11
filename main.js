29bd27c10f73c77e1f5ab49f8e6d7bba9abbc5b4
edanvagu <edward_avg@hotmail.com>
2022-08-11 00:21:33 -0500

    merge mobile menu and shopping cart

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
+//querySelectors
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-navbarEmail.addEventListener('click',toggleMenu);
+const menuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.product-detail');
+const backArrowIcon = document.querySelector('.back-arrow-icon');
 
-function toggleMenu(){
+//eventListeners
+navbarEmail.addEventListener('click',toggleMenuDesktop); // listener to toggle desktop menu
+menuIcon.addEventListener('click', toggleMobileMenu); // listener to toggle mobile menu
+shoppingCartIcon.addEventListener('click',toggleShoppingCart); // listener shopping cart
+backArrowIcon.addEventListener('click',toggleShoppingCart); // listener shopping cart
+
+//functions
+function toggleMenuDesktop(){ //toggle menu in desktop mode
+    shoppingCart.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){ //toggle menu in mobile mode
+    shoppingCart.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleShoppingCart(){ //toggle shopping cart
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    shoppingCart.classList.toggle('inactive');
 }
