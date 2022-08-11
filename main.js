f4e31945168285b9cacfd13b7f6ffc112a444adc
edanvagu <edward_avg@hotmail.com>
2022-08-10 22:01:07 -0500

    merge main and desktop and mobile menu html/css

---
+++ main.js
@@ -0,0 +1,7 @@
+const navbarEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+navbarEmail.addEventListener('click',toggleMenu);
+
+function toggleMenu(){
+    desktopMenu.classList.toggle('inactive');
+}
